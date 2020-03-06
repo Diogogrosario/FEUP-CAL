8eb1637c0e6105df1b539a46f62c5e6b35e48317
Diogo Rosário <diogogrosario1008@gmail.com>
2020-03-06 16:31:49 +0000

    Finished TP1

--- TP1+2/Tests/Partitioning.cpp
+++ TP1+2/Tests/Partitioning.cpp
@@ -3,27 +3,49 @@
  */
 
 #include "Partitioning.h"
+#include <vector>
 
+using namespace std;
 
 int s_recursive(int n,int k)
 {
-   return 1;
+   if( k == n || k == 1){
+       return 1;
+   }
+   return (s_recursive(n-1,k-1)+k*s_recursive(n-1,k));
 }
 
 int s_dynamic(int n,int k)
 {
-	return 1;
+    vector <int> values;
+    for(int i = 0;i<n-k;i++){
+        for(int j = 1;j<k;j++){
+            values.at(j) = (j+1)*values.at(j)+values.at(j-1);
+        }
+    }
+    return values.at(k-1);
+
 }
 
 
 int b_recursive(int n)
 {
-	return 0;
+    int result = 0;
+    for(int i = 1;i<=n;i++)
+        result += s_recursive(n,i);
+	return result;
 }
 
 int b_dynamic(int n)
 {
-	return 0;
+    int result = 0;
+    for(int i = 1;i<=n;i++)
+        result += s_dynamic(n,i);
+    return result;
 }
+
+
+
+
 
 
